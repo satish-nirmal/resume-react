701a35ed8279cada709b712ff7dda42a72e8a951
satish-nirmal <satishnirmal16@gmail.com>
2024-06-25 22:26:35 +0530

    created sub-branch in the master

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <p>+917350161638 | <a href="mailto:satishnirmal2130@gmail.com">satishnirmal2130@gmail.com</a></p>
       <p>
         <a href="https://github.com/satish-nirmal">GitHub</a> | 
-        <a href="https://www.linkedin.com/in/satish-nirmal/"> LinkedIn</a> | 
+        <a href="https://www.linkedin.com/in/satish-nirmal/">LinkedIn</a> | 
         <a href="https://leetcode.com/satisfy_7/"> LeetCode</a>
       </p>
     </header>
