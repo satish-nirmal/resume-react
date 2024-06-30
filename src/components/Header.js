2d94662b71371d9ef3cada5377657a9d008eb5eb
satish-nirmal <satishnirmal16@gmail.com>
2024-06-30 21:22:10 +0530

    changed the headers name for github, leetcode and linkedin

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ const Header = () => {
       <p>Rayalaseema pg hinjewadi, Pune, Maharashtra, India</p>
       <p>+917350161638 | <a href="mailto:satishnirmal2130@gmail.com">satishnirmal2130@gmail.com</a></p>
       <p>
-        <a href="https://github.com/satish-nirmal">GitHub</a> | 
-        <a href="https://www.linkedin.com/in/satish-nirmal/">LinkedIn</a> | 
-        <a href="https://leetcode.com/satisfy_7/"> LeetCode</a>
+        <a href="https://github.com/satish-nirmal">Satish Nirmal</a> | 
+        <a href="https://www.linkedin.com/in/satish-nirmal/">Satish Nirmal</a> | 
+        <a href="https://leetcode.com/satisfy_7/"> satisfy.7</a>
       </p>
     </header>
   );
