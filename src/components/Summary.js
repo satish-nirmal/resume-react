2939250f349f1b806e66915929cb16653af068bf
satish-nirmal <satishnirmal16@gmail.com>
2024-07-22 22:52:05 +0530

    updated the summery

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -5,10 +5,7 @@ const Summary = () => {
     <section>
       <h2>Summary</h2>
       <p>
-        Full Stack Developer with around 2 years of experience specializing in Java, Spring Boot, and React. Proven ability to
-        leverage full-stack expertise to build interactive and user-centered website designs to scale. Extensive experience
-        working in startups and large-scale organizations, including Heaptrace teachnology. Also contributed to open-source
-        projects like cal.com.
+      Full Stack Developer with 2 years of experience specializing in Java, Spring Boot, and React. Proven ability to leverage full-stack expertise to build interactive, user-centered websites that scale. Extensive experience in both startups and large-scale organizations.
       </p>
     </section>
   );
